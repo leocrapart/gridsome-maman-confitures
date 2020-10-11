746a9e83473a72be4d299b84fb20a772da505bc9
Leo Crapart <leocrapart@yahoo.fr>
2020-10-11 18:41:34 +0200

    new tailwindway

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,19 +1,9 @@
-const tailwind = require("tailwindcss");
-const purgecss = require("@fullhuman/postcss-purgecss");
-
-const postcssPlugins = [tailwind()];
-
-if (process.env.NODE_ENV === "production")
-  postcssPlugins.push(purgecss(require("./purgecss.config.js")));
 
 module.exports = {
   siteName: "Gridsome",
-  plugins: [],
-  css: {
-    loaderOptions: {
-      postcss: {
-        plugins: postcssPlugins
-      }
+  plugins: [
+    {
+      use: "gridsome-plugin-tailwindcss"
     }
-  }
+  ],
 };
